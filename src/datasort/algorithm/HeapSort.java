982100d2a6b83aea7348bf5174925008ba533a7e
William Barbosa <williampmb@gmail.com>
2017-09-17 23:53:51 -0300

    - Changed to BinaryHeap In-place

--- src/datasort/algorithm/HeapSort.java
+++ src/datasort/algorithm/HeapSort.java
@@ -17,10 +17,21 @@ public class HeapSort extends Sort {
     public <T extends Comparable<T>> T[] execute(T[] array) {
         MyHeap myHeap = MyHeap.buildHeapFromArray(array);
         
+        /*Heapsort in-place. O objeto MyHeap compartilha o mesmo array que o
+        * array de entrada. Neste array, dentro da classe o MyHeap, para cada 
+        * elemento de 0 até array.length-1, ele retira o elemento do array e
+        * insere no array ajustando o heap conforme necesseário. No fim, para manter
+        * a ordem, ele chama N vezes o min() do heap retirando em ordem crescente
+        * os elementos e organizando numa ordem invertida do array para não
+        * sobrescrever os elementos do array.
+        */
         for(int i =0 ; i < array.length; i++){
-            array[i] = (T) myHeap.min();
+            array[array.length-1-i] = (T) myHeap.min();
         }
-        
+        //Como o array está em ordem decrescente, é necessário inverter a ordem
+        // do array.
+        reverseArray(array);
+     
         return array;
     }
     
